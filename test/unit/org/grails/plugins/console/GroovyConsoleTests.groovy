2bded22b94c2eccec3cbec64a3b4993d44c96ef1
Mingfai Ma <mingfai.ma@gmail.com>
2009-02-20 14:03:05 +0000

    added test case

---
+++ test/unit/org/grails/plugins/console/GroovyConsoleTests.groovy
@@ -0,0 +1,13 @@
+package org.grails.plugins.console
+
+public class GroovyConsoleTests extends GroovyTestCase {
+  void testBinding() {
+    def bindings = new Binding('foo0': 'bar0')
+    def shell = new GroovyShell(bindings)
+    assertEquals 'bar0', shell.evaluate("return foo0")
+    shell.evaluate("foo1 = 'bar1'") //without 'def', a variable can be bounded
+    assertNotNull bindings.variables.find {k, v -> k == 'foo1'}
+    shell.evaluate("def foo2 = 'bar2'") //with 'def' keyword, the variable fail to bind
+    assertNull bindings.variables.find {k, v -> k == 'foo2'}
+  }
+}
